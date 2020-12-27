d63ace810b8697458d18532ca93115a4661283d3
Lundez <hampus.londogard@gmail.com>
2020-12-27 19:58:45 +0100

    Adding CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `maven-publish`
     kotlin("jvm") version "1.4.21"
     kotlin("plugin.serialization") version "1.4.21"
+    id("org.jetbrains.dokka") version "1.4.20"
 }
 
 group = "com.londogard"
@@ -10,13 +12,18 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
     maven("https://jitpack.io")
 }
+
+val kluentVersion: String by project
 
 dependencies {
     // ND4J
     implementation("org.nd4j:nd4j-api:1.0.0-beta7")
 
+    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.4.21")
     testImplementation(kotlin("test-junit"))
 }
 
@@ -24,6 +31,24 @@ tasks.test {
     useJUnit()
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/londogard/londogard-nlp-toolkit")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
