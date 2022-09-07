8da57d04ea695c693a45277bd0d6fdf3fcb469aa
Lundez <hampus.londogard@gmail.com>
2022-09-07 06:43:17 +0200

    further docs

--- src/main/kotlin/com/londogard/nlp/meachinelearning/encoders/Encoder.kt
+++ src/main/kotlin/com/londogard/nlp/meachinelearning/encoders/Encoder.kt
@@ -6,11 +6,11 @@ import org.jetbrains.kotlinx.multik.ndarray.data.*
  * [Encoder] is a way to encode something into numerical categories
  */
 interface Encoder<T> {
-    fun fit(input: D1Array<T>): Unit
-    fun transform(input: D1Array<T>): D2Array<Int>
-    fun fitTransform(input: D1Array<T>): D2Array<Int> {
+    fun fit(input: List<T>): Unit
+    fun transform(input: List<T>): D2Array<Int>
+    fun fitTransform(input: List<T>): D2Array<Int> {
         fit(input)
         return transform(input)
     }
-    fun invert(input: D2Array<Int>): D1Array<T>
+    fun invert(input: D2Array<Int>): List<T>
 }
--- src/main/kotlin/com/londogard/nlp/meachinelearning/encoders/OneHotEncoder.kt
+++ src/main/kotlin/com/londogard/nlp/meachinelearning/encoders/OneHotEncoder.kt
@@ -1,31 +1,32 @@
 package com.londogard.nlp.meachinelearning.encoders
 
-import org.jetbrains.kotlinx.multik.api.d1array
 import org.jetbrains.kotlinx.multik.api.mk
 import org.jetbrains.kotlinx.multik.api.zeros
 import org.jetbrains.kotlinx.multik.ndarray.data.*
-import org.jetbrains.kotlinx.multik.ndarray.operations.forEachIndexed
-import org.jetbrains.kotlinx.multik.ndarray.operations.max
 import kotlin.properties.Delegates
 
+/**
+ * A simple OneHotEncoder which encodes a category of any type into a numeric value.
+ */
 class OneHotEncoder<T>: Encoder<T> {
     private var mapping by Delegates.notNull<Map<T, Int>>()
-    private val reverseMapping by Delegates.notNull<D1Array<T>>()
+    private var reverseMapping by Delegates.notNull<List<T>>()
 
-    override fun fit(input: D1Array<T>) {
-        val data = input.data.distinct()
-        mapping = data.withIndex().associate { it.value to it.index }
-        reverseMapping = mk.d1array<T::class>(data.size) { i -> data[i] }
+    override fun fit(input: List<T>) {
+        reverseMapping = input.distinct()
+        mapping = reverseMapping.withIndex().associate { it.value to it.index }
     }
 
-    override fun transform(input: D1Array<T>): D2Array<Int> {
-        val out = mk.zeros<Int>(input.shape[0], mapping.size)
+    override fun transform(input: List<T>): D2Array<Int> {
+        val out = mk.zeros<Int>(input.size, mapping.size)
         input.forEachIndexed { index, item -> out[index, mapping.getValue(item)] = 1 }
 
         return out
     }
 
-    override fun invert(input: D2Array<Int>): D1Array<T> {
-        return mk
+    override fun invert(input: D2Array<Int>): List<T> {
+        val categories = mk.math.argMaxD2(input, 1)
+
+        return List(categories.size) { i -> reverseMapping[i] }
     }
 }
